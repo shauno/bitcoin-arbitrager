1c6ebb503adb9fa1c8ba0e9098d62736fd0d3bfb
Shaun Alberts <shaunalberts@gmail.com>
2017-07-27 21:27:15 +0200

    Add hacked in line to show arbitrage percent

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -19,30 +19,56 @@
     jQuery.ajax({
         'url': '/api/v1/current-rates',
         'success': function(data) {
-            var exchanges = [], labels = [];
+            var exchanges = [], labels = [], buy = {}, sell = {};
 
-            for (i in data) {
+            for (var i in data) {
                 var rates = [];
 
                 for(rate in data[i].current_rate) {
                     var key = data[i].current_rate[rate];
-                    //console.log(key);
                     rates[rates.length] = {x: key.created_at, y: key.rate};
-                    if ( jQuery.inArray( key.created_at, labels ) == -1 ) {
+                    if ( jQuery.inArray( key.created_at, labels ) === -1 ) {
                         labels[labels.length] = key.created_at;
+                    }
+
+                    if(data[i].from_iso === 'XBT') {
+                        if (typeof buy[key.created_at] === "undefined" || key.rate < buy[key.created_at]) {
+                            buy[key.created_at] = key.rate;
+                        }
+                    }
+
+                    if(data[i].from_iso === 'ZAR') {
+                        if (typeof sell[key.created_at] === "undefined" || key.rate > sell[key.created_at]) {
+                            sell[key.created_at] = key.rate;
+                        }
                     }
                 }
 
-
-                var dash = data[i].from_iso == 'ZAR' ? 5 : 0;
+                var dash = data[i].from_iso === 'ZAR' ? 5 : 0; // 5 = dash (sell), 0 = solid (buy)
                 exchanges[exchanges.length] = {
                     label: data[i].exchange.name + ' ' + data[i].from_iso + ':' + data[i].to_iso ,
                     yAxisID: 'y-axis-rand',
                     data: rates,
                     fill: false,
-                    borderDash: [5, dash]
+                    borderDash: [5, dash],
+                    borderColor: '#216C2A', //todo, exchange related colours
+                    backgroundColor: '#216C2A'
                 };
             }
+
+            var gap = [];
+            for(created_at in buy) {
+                if (typeof sell[created_at] !== "undefined") {
+                    gap[gap.length] = ((sell[created_at] - buy[created_at]) / sell[created_at]) * 100;
+                }
+            }
+
+            exchanges[exchanges.length] = {
+                label: 'Arbitrage gap',
+                yAxisID: 'y-axis-percent',
+                data: gap,
+                fill: false
+            };
 
             new Chart(document.getElementById("chart-zar-arbitrage"), {
                 type: 'line',
