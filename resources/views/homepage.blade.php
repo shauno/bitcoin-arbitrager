db6e01310913fe10954433dc7e0a81f22feca492
Shaun Alberts <shaunalberts@gmail.com>
2017-07-28 21:25:23 +0200

    Improve the graph to handle missing data points

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -16,39 +16,53 @@
 <script src="/js/jquery-3.2.1.min.js"></script>
 
 <script>
+    function convertObjToArray(obj, labels) {
+        var array = [];
+
+        for(date in labels) {
+            array[array.length] = typeof obj[date] === 'undefined' ? null : obj[date];
+        }
+
+        return array;
+    }
+
     jQuery.ajax({
         'url': '/api/v1/current-rates',
         'success': function(data) {
-            var exchanges = [], labels = [], buy = {}, sell = {};
+            var exchanges = [], labels = {}, buy = {}, sell = {};
 
-            for (var i in data) {
-                var rates = [];
+            for(var exchange in data) {
+                var rates = {};
+                for(rate in data[exchange].current_rate) {
+                    var current_rate = data[exchange].current_rate[rate]; //just a shorter var name
 
-                for(rate in data[i].current_rate) {
-                    var key = data[i].current_rate[rate];
-                    rates[rates.length] = {x: key.created_at, y: key.rate};
-                    if ( jQuery.inArray( key.created_at, labels ) === -1 ) {
-                        labels[labels.length] = key.created_at;
-                    }
+                    //set lall the labels
+                    labels[current_rate.created_at] = current_rate.created_at;
 
-                    if(data[i].from_iso === 'XBT') {
-                        if (typeof buy[key.created_at] === "undefined" || key.rate < buy[key.created_at]) {
-                            buy[key.created_at] = key.rate;
+                    //set the rate for this exchange
+                    rates[current_rate.created_at] = current_rate.rate;
+
+                    //check if this buy rate is better than any before
+                    if(data[exchange].from_iso === 'XBT') {
+                        if (typeof buy[current_rate.created_at] === "undefined" || current_rate.rate < buy[current_rate.created_at]) {
+                            buy[current_rate.created_at] = current_rate.rate;
                         }
                     }
 
-                    if(data[i].from_iso === 'ZAR') {
-                        if (typeof sell[key.created_at] === "undefined" || key.rate > sell[key.created_at]) {
-                            sell[key.created_at] = key.rate;
+                    //check if this sell rate is better than any before
+                    if(data[exchange].from_iso === 'ZAR') {
+                        if (typeof sell[current_rate.created_at] === "undefined" || current_rate.rate > sell[current_rate.created_at]) {
+                            sell[current_rate.created_at] = current_rate.rate;
                         }
                     }
                 }
 
-                var dash = data[i].from_iso === 'ZAR' ? 5 : 0; // 5 = dash (sell), 0 = solid (buy)
+                //add the exchange line (the rates object needs to be converted to an array still)
+                var dash = data[exchange].from_iso === 'ZAR' ? 5 : 0; // 5 = dash (sell), 0 = solid (buy)
                 exchanges[exchanges.length] = {
-                    label: data[i].exchange.name + ' ' + data[i].from_iso + ':' + data[i].to_iso ,
+                    label: data[exchange].exchange.name + ' ' + data[exchange].from_iso + ':' + data[exchange].to_iso ,
                     yAxisID: 'y-axis-rand',
-                    data: rates,
+                    data: rates, //we can't convert it to array yet because we might not have all the label keys yet
                     fill: false,
                     borderDash: [5, dash],
                     borderColor: '#216C2A', //todo, exchange related colours
@@ -56,24 +70,41 @@
                 };
             }
 
-            var gap = [];
-            for(created_at in buy) {
-                if (typeof sell[created_at] !== "undefined") {
-                    gap[gap.length] = ((sell[created_at] - buy[created_at]) / sell[created_at]) * 100;
+            //sort the labels. If some exchanges are missing dates the labels can get added with the odd point out of order
+            Object.keys(labels).sort().forEach(function(key) {
+                var value = labels[key];
+                delete labels[key];
+                labels[key] = value;
+            });
+
+            //convert each exchanges data object to array
+            for(var i=0; i<exchanges.length; i++) {
+                exchanges[i].data = convertObjToArray(exchanges[i].data, labels);
+            }
+
+            //work out the best arbitrage gap (if we have both buy and sell data for each date)
+            var gap = {};
+            for(date in labels) {
+                if(typeof buy[date] !== "undefined" && typeof sell[date] !== "undefined") {
+                    gap[date] = ((sell[date] - buy[date]) / sell[date]) * 100;
+                }else{
+                    gap[date] = null;
                 }
             }
 
+            //add the arbitrage line to the graph
             exchanges[exchanges.length] = {
                 label: 'Arbitrage gap',
                 yAxisID: 'y-axis-percent',
-                data: gap,
+                data: convertObjToArray(gap, labels),
                 fill: false
             };
 
+            //make it so
             new Chart(document.getElementById("chart-zar-arbitrage"), {
                 type: 'line',
                 data: {
-                    labels: labels,
+                    labels: convertObjToArray(labels, labels),
                     datasets: exchanges
                 },
                 options: {
